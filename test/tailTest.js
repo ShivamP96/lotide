9de771cfb253779e239969e5d47c53c880d24a0c
Shivam <patelsd96@gmail.com>
2020-01-13 20:40:16 +0000

    worked on headTest to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -6,3 +6,7 @@ const assertEqual = require("../assertEqual");
 const words = [1,2,3,4];
 console.log(tail(words));
 assertEqual(tail(words).length, 3); // original array should still have 3 elements!
+
+
+const chai = require('chai').assert;
+const tail = require('../tail');
