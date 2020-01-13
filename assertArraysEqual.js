8973599d3b80e078759f80073628945467bd40ec
Shivam <patelsd96@gmail.com>
2020-01-13 19:43:26 +0000

    Refactored middle to include require and export and made middleTest that works

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function (actual, expected){
+const assertArraysEqual = function (actual, expected) {
 
   if (eqArrays(actual, expected)) {
     console.log(` ✅ Assertion Passed: ${actual}  ===  ${expected}`);
--- middle.js
+++ middle.js
@@ -1,25 +1,6 @@
-const eqArrays = function (array1, array2){
 
-  if (array1.length !== array2.length) {
-    return false;
-  } 
-  for (let i = 0; i < array2.length; i++) {
-    if (array1[i] !== array2[i]){
-      return false;
-    }
-  }
-  return true;
-};
-
-
-const assertArraysEqual = function (actual, expected){
-
-  if (eqArrays(actual, expected)) {
-    console.log(` ✅ Assertion Passed: ${actual}  ===  ${expected}`);
-  } else {
-    console.log(` 🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual')
 
 
 const middle = function (array){
@@ -31,7 +12,7 @@ if (array.length === 1 || array.length === 2){
 } else if (array.length % 2 === 1){
     output.push(array[middle]);  
 
-} else if (array.length %2 === 0){
+} else if (array.length % 2 === 0){
    output.push(array[middle -1]);
    output.push(array[middle]);
 } 
@@ -45,3 +26,5 @@ assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
+
+module.exports = middle;
